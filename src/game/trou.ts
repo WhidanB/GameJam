36e41fb95b7ed397b1ec7472c5d16e5b7b18b057
WhidanB <danyphone95@gmail.com>
2023-07-06 14:44:06 +0200

    clone

--- src/game/trou.ts
+++ src/game/trou.ts
@@ -1,29 +1,35 @@
-import Phaser from "phaser";
+import Phaser, { Scene } from "phaser";
+import Game from "../Game";
 import TextureKeys from "../consts/TextureKeys";
 import AnimationKeys from "../consts/AnimationKeys";
-import Bear from "./Bear";
 
 export default class Trou extends Phaser.GameObjects.Container {
-  private trou: Phaser.GameObjects.Sprite;
+    private trou: Phaser.GameObjects.Sprite;
 
-  constructor(scene: Phaser.Scene, x: number, y: number, bear: Bear) {
-    super(scene, x, y);
+    constructor(scene: Phaser.Scene, x: number, y: number) {
+        super(scene, x, y);
+    
+        // Get CursorKeys Instance
 
-    this.trou = scene.add.sprite(0, 0, TextureKeys.Trou).setOrigin(0.5, 1);
+    
+        // Create a rocketbear Sprite:
+        this.trou = scene.add
+          .sprite(0, 0, TextureKeys.Trou)
+          .setOrigin(0.5, 1)
+    
+        // Add the Rocketbear as a child of Container:
+        this.add(this.trou);
+    
+        // Add a physics body:
+        scene.physics.add.existing(this);
+    
+        // Adjust physics body size and offset:
+        const body = this.body as Phaser.Physics.Arcade.Body;
+        body.setSize(80, 40);
+        body.setOffset(-40, -40);
+    
+  
 
-    this.add(this.trou);
+      }
 
-    scene.physics.add.existing(this);
-
-    const body = this.body as Phaser.Physics.Arcade.Body;
-    body.setSize(80, 40);
-    body.setOffset(-40, -40);
-
-    scene.physics.add.collider(this, bear, () => {
-
-      bear.destroy();
-
-      bear.setPosition(0, 0);
-    });
-  }
 }
